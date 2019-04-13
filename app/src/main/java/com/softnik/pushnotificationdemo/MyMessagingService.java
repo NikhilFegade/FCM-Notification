b28f0ccd3c32a1c36d404e36eb02d1f8bf820ab4
Nikhil Fegade <nikhilfegade17@gmail.com>
2019-04-13 15:18:33 +0530

    first commit

---
+++ app/src/main/java/com/softnik/pushnotificationdemo/MyMessagingService.java
@@ -0,0 +1,30 @@
+package com.softnik.pushnotificationdemo;
+
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        showNotifications(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+
+    }
+
+    public void showNotifications(String title,String message){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"MyNotifications")
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_stat_icon)
+                .setAutoCancel(true)
+                .setContentText(message);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(999,builder.build());
+    }
+}
